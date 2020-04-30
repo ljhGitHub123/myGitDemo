16be469c9fb43ff6d1cf65119f32a5fbbbde7574
ljhGitHub123 <1204263783@qq.com>
2020-04-30 12:42:16 +0800

    这是我的第一个上传到GitHub的项目

---
+++ src/main/java/com/roy/App.java
@@ -0,0 +1,13 @@
+package com.roy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "这是第一个上传到GitHub的项目" );
+    }
+}
